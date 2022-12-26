34608d55c75cd42b2684042ba0563cf1fb2e6b1f
chan haga <obutora@gmail.com>
2022-12-26 22:17:31 +0900

    wip : test latest update

--- qwik/src/routes/index.tsx
+++ qwik/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import S_card from "~/components/card/s_card";
+import Latest_update from "~/components/information/latest_update";
 
 export default component$(() => {
   return (
@@ -13,7 +14,9 @@ export default component$(() => {
           <span class="underline">更新は1日に1回</span>
           のため、最新の在庫とは異なることをご了承ください。
         </p>
-        <div>{/* <Latest_update /> */}</div>
+        <div>
+          <Latest_update />
+        </div>
       </S_card>
 
       {/* <Search_med_card /> */}
